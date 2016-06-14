ccfe6b9e9d1aebf0145c1c38c59122b713485a39
lujianrong <lujianrong_ios@126.com>
2016-06-14 17:51:34 +0800

    添加异步请求

--- PersonModelTests/PersonModelTests.m
+++ PersonModelTests/PersonModelTests.m
@@ -65,4 +65,43 @@
     UIImage *scaledImage = [originImage scaleToSize:CGSizeMake(120, 160)];
     XCTAssertEqual(scaledImage.size.width == 120., scaledImage.size.height==160., @"压缩不正确!");
 }
+
+/**
+ XCTest提供了XCTestExpectation来进行异步测试，即可在指定时机使用其fulfill实例方法开始执行断言命令。例如以下是一个完整的网络请求的测试样例，可用于测试server端是否正常，要在对应的block中进行断言
+ */
+- (void)testAsynchronousURLConnection {
+    XCTestExpectation *expectation = [self expectationWithDescription:@"GET Baidu"];
+    
+    NSURL *url = [NSURL URLWithString:@"http://www.baidu.com/"];
+    NSURLSession *session = [NSURLSession sharedSession];
+    NSURLSessionDataTask *task = [session dataTaskWithURL:url completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        // XCTestExpectation条件已满足，接下来的测试可以执行了。
+        [expectation fulfill];
+        XCTAssertNotNil(data, @"返回数据不应非nil");
+        XCTAssertNil(error, @"error应该为nil");
+        if (nil != response) {
+            NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)response;
+            XCTAssertEqual(httpResponse.statusCode, 200, @"HTTPResponse的状态码应该是200");
+            XCTAssertEqual(httpResponse.URL.absoluteString, url.absoluteString, @"HTTPResponse的URL应该与请求的URL一致");
+        } else {
+            XCTFail(@"返回内容不是NSHTTPURLResponse类型");
+        }
+    }];
+    [task resume];
+    
+    // 超时后执行
+    [self waitForExpectationsWithTimeout:10 handler:^(NSError * _Nullable error) {
+        [task cancel];
+    }];
+}
 @end
+
+
+
+
+
+
+
+
+
+
