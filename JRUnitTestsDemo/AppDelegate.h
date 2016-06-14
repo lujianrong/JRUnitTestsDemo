0ec9db4c1b3c00b8b2df00f5a56c238ee3cd87ea
lujianrong <lujianrong_ios@126.com>
2016-06-14 16:38:12 +0800

    Initial Commit

---
+++ JRUnitTestsDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JRUnitTestsDemo
+//
+//  Created by lujianrong on 16/6/14.
+//  Copyright © 2016年 JR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
